af3dcae10f95abf6d3e5214a63ac522452cc8724
akihirotomizawa <akihirowwzai04a@gmail.com>
2023-10-15 15:17:05 +0900

    Initial commit

---
+++ app/src/main/java/com/memo/unsplashclient/data/remote/ProfileImage.kt
@@ -0,0 +1,11 @@
+package com.memo.unsplashclient.data.remote
+
+
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class ProfileImage(
+    val large: String?,
+    val medium: String?,
+    val small: String?
+)
