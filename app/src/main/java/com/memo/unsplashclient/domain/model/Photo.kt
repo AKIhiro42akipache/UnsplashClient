f411e3da8ea22e7819d69139781d58e629c38587
akihirotomizawa <akihirowwzai04a@gmail.com>
2023-10-15 17:26:32 +0900

    画像表示、画像詳細表示それぞれのモデルクラスを追加しました。

--- app/src/main/java/com/memo/unsplashclient/domain/model/Photo.kt
+++ app/src/main/java/com/memo/unsplashclient/domain/model/Photo.kt
@@ -1,3 +1,14 @@
 package com.memo.unsplashclient.domain.model
 
-data class Photo()
+data class Photo(
+    //画像ID
+    val photoId:String,
+    //画像のタイトル
+    val description:String?,
+    //画像へのlikeの数
+    val likes:Int?,
+    //画像のリンク
+    val imageUrl:String,
+    //撮影者のユーザー名
+    val photographer:String?,
+)
--- app/src/main/java/com/memo/unsplashclient/domain/model/PhotoDetail.kt
+++ app/src/main/java/com/memo/unsplashclient/domain/model/PhotoDetail.kt
@@ -1,3 +1,18 @@
 package com.memo.unsplashclient.domain.model
 
-data class PhotoDetail()
+data class PhotoDetail(
+    //画像の説明
+    val description:String?,
+    //likeの数
+    val likes:Int?,
+    //画像のURL
+    val imageUrl:String,
+    //撮影者のユーザ名
+    val photographer:String?,
+    //撮影したカメラの名前
+    val camera:String?,
+    //撮影場所
+    val location:String?,
+    //Unsplashでダウンロードした回数
+    val downloads:Int?,
+)
