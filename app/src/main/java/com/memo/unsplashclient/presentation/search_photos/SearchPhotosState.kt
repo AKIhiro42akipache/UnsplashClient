a0947098d913294a47590c27ca2e5f843b1c6ec2
akihirotomizawa <akihirowwzai04a@gmail.com>
2023-10-17 22:34:46 +0900

    Presentation層で値を保持するstateとViewModelを作成し、ネットワークの状態に応じて処理を分岐できるようにwhen式で記述しました。

--- app/src/main/java/com/memo/unsplashclient/presentation/search_photos/SearchPhotosState.kt
+++ app/src/main/java/com/memo/unsplashclient/presentation/search_photos/SearchPhotosState.kt
@@ -1,3 +1,9 @@
 package com.memo.unsplashclient.presentation.search_photos
 
-data class SearchPhotosState()
+import com.memo.unsplashclient.domain.model.Photo
+
+data class SearchPhotosState(
+    val isLoading:Boolean = false,
+    val photos:List<Photo> = emptyList(),
+    val error:String? = null
+)
--- app/src/main/java/com/memo/unsplashclient/presentation/search_photos/SearchPhotosViewModel.kt
+++ app/src/main/java/com/memo/unsplashclient/presentation/search_photos/SearchPhotosViewModel.kt
@@ -1,4 +1,54 @@
 package com.memo.unsplashclient.presentation.search_photos
 
-class SearchPhotosViewModel {
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.memo.unsplashclient.common.NetworkResponse
+import com.memo.unsplashclient.domain.use_case.SearchPhotosUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import javax.inject.Inject
+
+@HiltViewModel
+/*ViewModelでは、画像検索機能をView側に表示する必要があるため、
+* SearchPhotosUseCaseへ依存する必要があるためconstructor()の引数に入力する*/
+class SearchPhotosViewModel @Inject constructor(
+    private val searchPhotosUseCase: SearchPhotosUseCase
+) :ViewModel(){
+    //画像検索画面(SearchPhotosState)の状態を管理するための変数
+    private val _state = mutableStateOf(SearchPhotosState())
+    //view側で状態を変更できるようにするためのstate
+    val state: State<SearchPhotosState> = _state
+
+    //アプリの起動時に最初に呼び出されるメソッド
+    init {
+        searchPhotos("Miyagi")
+    }
+
+    //画像検索機能のメソッド
+    fun searchPhotos(query: String) {
+        //Invoke関数を呼び出す
+        //ネットワークresponseの状態が変わるたびに呼び出されるためそれぞれのケースの処理を記述
+        searchPhotosUseCase(query).onEach { result ->
+            when (result) {
+                //通信成功時の処理
+                is NetworkResponse.Success -> {
+                    _state.value = SearchPhotosState(
+                        isLoading = false,
+                        photos = result.data ?: emptyList()
+                    )
+                }
+                //エラーが発生したときの処理
+                is NetworkResponse.Failure -> {
+                    _state.value = SearchPhotosState(error = result.error)
+                }
+                //ロード中の処理
+                is NetworkResponse.Loading -> {
+                    _state.value = SearchPhotosState(isLoading = true)
+                }
+            }
+        }.launchIn(viewModelScope)
+    }
 }
