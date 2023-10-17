7c518bf3eb2b5787f67f3c1e4ac212f9d911535b
akihirotomizawa <akihirowwzai04a@gmail.com>
2023-10-17 22:32:30 +0900

    buildgrardleでnavigationによる画面遷移ができるように依存関係を追加し、MainActivityをpresentation層に移動させたため、 一部記述を書き換えました。

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,11 +7,12 @@ plugins {
 
 android {
     namespace = "com.memo.unsplashclient"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.memo.unsplashclient"
         minSdk = 26
+        //noinspection OldTargetApi
         targetSdk = 33
         versionCode = 1
         versionName = "1.0"
@@ -32,11 +33,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
@@ -52,8 +53,8 @@ android {
 }
 
 dependencies {
-
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.navigation:navigation-compose:2.7.4")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.8.0")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
