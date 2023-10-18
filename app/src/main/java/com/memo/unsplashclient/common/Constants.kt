48ed05cb971fe5309861bb6c758fb07752dfc00e
akihirotomizawa <akihirowwzai04a@gmail.com>
2023-10-18 15:26:47 +0900

    UnSplashAPIのエントリーポイントとなるURLを追加しました。

--- app/src/main/java/com/memo/unsplashclient/common/Constants.kt
+++ app/src/main/java/com/memo/unsplashclient/common/Constants.kt
@@ -2,4 +2,5 @@ package com.memo.unsplashclient.common
 
 object Constants {
     const val API_KEY = "kjbtE9EmOCZCfOvsLSDDDA2JLzlvrP6dcV9Xm7slrp8"
+    const val BASE_URL = "https://api.unsplash.com"
 }
