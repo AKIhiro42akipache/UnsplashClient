c3966ddbda5c3aedc973e266403efa3f4f1fb794
akihirotomizawa <akihirowwzai04a@gmail.com>
2023-10-19 17:08:12 +0900

    stateからローディングの状態を取得し、それぞれのstatusに併せて処理を分岐できるように 実装いたしました。

--- app/src/main/java/com/memo/unsplashclient/presentation/search_photos/SearchPhotosScreen.kt
+++ app/src/main/java/com/memo/unsplashclient/presentation/search_photos/SearchPhotosScreen.kt
@@ -1,12 +1,20 @@
 package com.memo.unsplashclient.presentation.search_photos
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.memo.unsplashclient.presentation.search_photos.components.PhotoThumbnail
 import com.memo.unsplashclient.presentation.search_photos.components.SearchBar
@@ -23,10 +31,33 @@ fun SearchPhotosScreen(viewModel:SearchPhotosViewModel = hiltViewModel()){
             onSearchChangeText = {viewModel.query = it},
             onDone = {viewModel.searchPhotos()})}
     ) {paddingValues ->
-        LazyColumn(modifier = Modifier.padding(paddingValues)){
-            items(state.photos) { photo ->
-                PhotoThumbnail(photo = photo, onClick = {})
+        //Boxを定義して画面幅いっぱいにローディングインジゲーターを設定する
+        Box(modifier = Modifier.fillMaxSize()){
+
+            when{
+                //画像検索画面のロードを行っている場合
+                state.isLoading ->{
+                    //ローディング中はローディングインジゲーターをBOX幅の中央に表示する
+                    CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+                }
+                !state.error.isNullOrBlank() ->{
+                    //エラー表示
+                    Text(
+                        text = state.error,
+                        modifier = Modifier.align(Alignment.Center),
+                        color = MaterialTheme.colorScheme.error
+                    )
+                }
+                else ->{
+                    LazyColumn(modifier = Modifier.padding(paddingValues)){
+                        items(state.photos) { photo ->
+                            PhotoThumbnail(photo = photo, onClick = {})
+                        }
+                    }
+                }
             }
+
         }
+
     }
 }
